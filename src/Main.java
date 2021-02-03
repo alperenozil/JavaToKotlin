38f6c57490236b763ada98e59e6b37b7099505a8
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-02-03 08:51:46 +0300

    main is updated

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,21 @@ import java.util.Scanner;
 
 public class Main {
     private static Map<Integer, Location> locations = new Locations().locations;
-
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
         int loc = 64;
-
         while (true) {
             System.out.println(locations.get(loc).getDescription());
-
             if (loc == 0) {
                 break;
             }
-
             Map<String, Integer> exits = locations.get(loc).getExits();
             System.out.print("Available exits are ");
             for (String exit: exits.keySet()) {
                 System.out.print(exit + ", ");
             }
             System.out.println();
-
             String direction = scanner.nextLine().toUpperCase();
-
             if (exits.containsKey(direction)) {
                 loc = exits.get(direction);
             }
@@ -33,6 +25,5 @@ public class Main {
                 System.out.println("You cannot go in that direction");
             }
         }
-
     }
 }
